630a284bb3043035d31bec35e5af8bbbff9f3e70
Mahin Jiwani <mahinjiwani@Mahins-MacBook-Air.local>
2019-03-30 13:07:35 -0400

    game

--- Assets/javascript/game.js
+++ Assets/javascript/game.js
@@ -2,21 +2,21 @@ alert("I'm thinking of a number 1-10. Guess what number I'm thinking of");
 
 var answer = 9
 // var guess;
-var guess = prompt("Guess the number");
+// var guess = ("Guess the number");
 
 
-for (i = 0; i < 10; i++) {
-    if (answer == guess) {
-        alert("You guessed correctly!");
-        break;
-    } else {
-        guess = prompt("Try again!");
-    }
-}
+// for (i = 0; i < 10; i++) {
+//     if (answer == guess) {
+//         alert("You guessed correctly!");
+//         break;
+//     } else {
+//         guess = prompt("Try again!");
+//     }
+// }
 
 var wins = 0;
 var losses = 0;
-
+var answers = null;
 
 var guessesLeft = 9;
 var guessesMax = 10;
@@ -46,7 +46,7 @@ yourGuessesSoFar.push(number);
     updateGuessesLeft();
     updateYourGuessesSoFar();
 
-    if (number === answer) {
+    if (number === answers) {
         wins++;
         document.querySelector("#wins").innerHTML = wins;
 
